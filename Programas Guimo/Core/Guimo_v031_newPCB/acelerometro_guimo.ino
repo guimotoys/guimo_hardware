f1b72d3a27aa1f77aa48cb7a0f30d5e16fa1b331
Thiago Henrique de Souza <Thiago Henrique de Souza>
2017-01-29 18:16:06 -0200

    Acrescentei o código do acelerômetro para identificar quando o Guimo está de ponta-cabeça

--- Programas Guimo/Core/Guimo_v031_newPCB/acelerometro_guimo.ino
+++ Programas Guimo/Core/Guimo_v031_newPCB/acelerometro_guimo.ino
@@ -10,15 +10,15 @@ void acelerometro()
       
           //Armazena o valor dos sensores nas variaveis correspondentes
           AcX=Wire.read()<<8|Wire.read(); //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-          //AcY=Wire.read()<<8|Wire.read(); //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+          AcY=Wire.read()<<8|Wire.read(); //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
           //AcZ=Wire.read()<<8|Wire.read(); //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
       
           //Mostra os valores na serial
           Bluetooth.print(AcX);  Bluetooth.println(";"); 
-          //Bluetooth.print(AcY);  Bluetooth.print(";"); 
+          Bluetooth.print(AcY);  Bluetooth.println(";"); 
           //Bluetooth.print(AcZ);  Bluetooth.println(";"); 
           Serial.print(AcX);  Serial.println(";"); 
-          //Serial.print(AcY);  Serial.print(";"); 
+          Serial.print(AcY);  Serial.print(";"); 
           //Serial.print(AcZ);  Serial.println(";"); 
      
           //Aguarda 300 ms e reinicia o processo
@@ -29,5 +29,11 @@ void acelerometro()
           {
             software_Reset();
           }
-          
+
+          //Identifica se o Guimo está de ponta cabeça
+          if(AcY < -10000)
+          {
+            Serial.println("inverso.bmp/n");
+          }
+}
 }
