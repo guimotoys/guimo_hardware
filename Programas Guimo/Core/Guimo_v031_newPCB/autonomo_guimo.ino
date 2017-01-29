d45a0d9a71ac8b3158260491b486686e0737fc03
Thiago Henrique de Souza <Thiago Henrique de Souza>
2017-01-29 17:18:00 -0200

    Programas corretos do Core. Recebendo e enviando os códigos de imagem BMP

---
+++ Programas Guimo/Core/Guimo_v031_newPCB/autonomo_guimo.ino
@@ -0,0 +1,33 @@
+//// Fun��o para identificar obst�culos (Guimo Aut�nomo)
+//
+//void guimo_Autonomo() {
+//  int distance, duration;
+//  digitalWrite(trigPin, HIGH);
+//  delay(200);
+//  digitalWrite(trigPin, LOW);
+//  duration = pulseIn(echoPin, HIGH);
+//  distance = (duration/2)/29.1;
+//  Serial.println("");
+//  Serial.println(distance);
+//  auto_Frente();
+//  if (distance < 22) {
+//    auto_Esquerda();
+//  }
+//}
+//
+//void auto_Frente() {
+//  iniciar_Motor();
+//  motorDireita.write(0);
+//  motorEsquerda.write(180);
+//  delay(150);
+//  Serial.println("Frente");
+//}
+//
+//void auto_Esquerda() {
+//  rotacao_Parado();
+//  iniciar_Motor();
+//  motorDireita.write(0);
+//  motorEsquerda.write(0);
+//  delay(150);
+//  Serial.println("Esquerda");
+//}
